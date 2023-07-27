6dd949fe8dad9e26f62b5ebeeaa94f68089ac11d
Lee <130075064+Isaac-Seungwon@users.noreply.github.com>
2023-07-27 12:16:38 +0900

    Add files via upload

--- Q056.java
+++ Q056.java
@@ -24,11 +24,9 @@ public class Q056 {
 		Scanner scan = new Scanner(System.in);
 
 		int num1, num2; // 입력한 숫자
-		int aliquot = 0; // 약수 합계 누적
-		String commonDivisor = ""; // 공약수
-		String aliquotListNum1 = ""; // 약수 리스트
-		String aliquotListNum2 = ""; // 약수 리스트
-		
+		String commonDevisor = ""; // 공약수
+		String devisorListNum1 = ""; // 약수 리스트
+		String devisorListNum2 = ""; // 약수 리스트
 		
 		// 숫자 2개 입력
 		System.out.print("첫번째 숫자: ");
@@ -36,50 +34,48 @@ public class Q056 {
 		System.out.print("두번째 숫자: ");
 		num2 = scan.nextInt();
 		
-		
 		// 약수 검증
-		aliquotListNum1 = getAliquot(num1, aliquotListNum1);
-		aliquotListNum2 = getAliquot(num2, aliquotListNum2);
+		devisorListNum1 = getdevisor(num1, devisorListNum1);
+		devisorListNum2 = getdevisor(num2, devisorListNum2);
 		
 		// 공약수 검증
-		commonDivisor = getCommonDivisor(num1, num2, commonDivisor);
-		
+		commonDevisor = getCommon(num1, num2, commonDevisor);
 		
 		// 출력
-		System.out.printf("%d의 약수: %s\n", num1, aliquotListNum1);
-		System.out.printf("%d의 약수: %s\n", num2, aliquotListNum2);
-		System.out.printf("%d와 %d의 공약수: %s\n", num1, num2, commonDivisor);
+		System.out.printf("%d의 약수: %s\n", num1, devisorListNum1);
+		System.out.printf("%d의 약수: %s\n", num2, devisorListNum2);
+		System.out.printf("%d와 %d의 공약수: %s\n", num1, num2, commonDevisor);
 	}
 
 	// 약수 검증 메소드
-	private static String getAliquot(int num, String aliquotList) {
+	private static String getdevisor(int num, String devisorList) {
 		for (int i = 1; i <= num; i++) {
-			aliquotList = "";
+			devisorList = "";
 					
 			for (int j = 1; j <= num; j++) {
 				// 약수 계산
 				if (i % j == 0) {
-					aliquotList = aliquotList + j + ", ";
+					devisorList = devisorList + j + ", ";
 				}
 			}
 		}
 		
-		return aliquotList;
+		return devisorList;
 	}
 	
 	// 공약수 검증 메소드
-	private static String getCommonDivisor(int num1, int num2, String commonDivisor) {
+	private static String getCommon(int num1, int num2, String common) {
 		for (int i = 1; i <= (num1 < num2 ? num1 : num2); i++) {
-			commonDivisor = "";
+			common = "";
 					
 			for (int j = 1; j < (num1 < num2 ? num1 : num2); j++) {
 				// 공약수 계산
 				if (i % j == 0) {
-					commonDivisor = commonDivisor + j + ", ";
+					common = common + j + ", ";
 				}
 			}
 		}
 		
-		return commonDivisor;
+		return common;
 	}
 }
